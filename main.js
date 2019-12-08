b5603a18f5f9d28c7224493fec5c1c0a8d7ec8f0
kellycao <55064994+kellycao@users.noreply.github.com>
2019-12-07 20:07:11 -0500

    menu_v2

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//title page wind blowing effect
 window.addEventListener("load",function(event){
   var texts=document.getElementsByClassName("letters");
   var numOfTexts = texts.length;
@@ -31,8 +32,13 @@ window.addEventListener("load",function(event){
     },200)
 });
 
+//menu hover effect
 var section = document.getElementsByClassName("section");
-var i = 0;
+var sectionName = document.getElementsByClassName("sectionName");
+
+for (var i = 0; i < section.length; i++) {
+  hoverSection(i);
+  }
 
 function hoverSection() {
   section[i].onmouseover = function() {
@@ -48,6 +54,34 @@ function hoverSection() {
   }
 }
 
-for (var i = 0; i < section.length; i++) {
-  hoverSection(i);
-}
+
+
+// switch(i){
+//   case 0:
+//   sectionName[2].style.visibility="visible";
+//   break;
+//   case 1:
+//   sectionName[0].style.visibility="visible";
+//   break;
+//   case 2:
+//   sectionName[3].style.visibility="visible";
+//   break;
+//   case 3:
+//   sectionName[1].style.visibility="visible";
+//   break;
+//   case 4:
+//   sectionName[4].style.visibility="visible";
+//   break;
+// }
+
+// if (i==0){
+//     sectionName[2].style.visibility="visible";
+// } else if (i==1) {
+//     sectionName[0].style.visibility="visible";
+// } else if (i==2) {
+//     sectionName[3].style.visibility="visible";
+// } else if (i==3) {
+//     sectionName[1].style.visibility="visible";
+// } else {
+//     sectionName[4].style.visibility="visible";
+// }
