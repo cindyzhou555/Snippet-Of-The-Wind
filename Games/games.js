7ceb6b78ec13457c9d88efbc52e6678e00fc10b3
kellycao <55064994+kellycao@users.noreply.github.com>
2019-12-14 09:24:54 -0500

    subpage_game_v1

---
+++ Games/games.js
@@ -0,0 +1,60 @@
+const TypeWriter = function(txtElement, words, wait){
+  this.txtElement=txtElement;
+  this.words=words;
+  this.txt='';
+  this.wordIndex = 0;
+  this.wait = parseInt(wait,10);
+  this.type();
+this.isDeleting=false;
+}
+
+//type method
+TypeWriter.prototype.type=function(){
+//current index of word
+const current = this.wordIndex % this.words.length;
+//get full text of current words
+const fullTxt = this.words[current];
+
+//check if isDeleting
+if(this.isDeleting){
+  //remove character
+  this.txt = fullTxt.substring(0, this.txt.length - 1)
+} else {
+  //add character
+  this.txt = fullTxt.substring(0, this.txt.length + 1)
+}
+
+//insert txt into txtElement
+this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+//type speed
+let typeSpeed = 200;
+if(this.isDeleting){
+  typeSpeed /= 2;
+}
+
+//if word is complete
+if(!this.isDeleting && this.txt === fullTxt){
+  //pause at end
+  typeSpeed = this.wait;
+  this.isDeleting = true;
+}
+ else if(this.isDeleting && this.txt === ''){
+  this.isDeleting = false;
+  //move to next word;
+  this.wordIndex++;
+  //pause before start typeing
+  typeSpeed = 400;
+}
+
+  setTimeout(()=>this.type(), typeSpeed);
+}
+//initialize onload
+document.addEventListener("DOMContentLoaded", init)
+
+function init(){
+  const txtElement = document.querySelector('.txt-type');
+  const words=JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  new TypeWriter(txtElement, words, wait);
+}
--- main.js
+++ main.js
@@ -32,59 +32,119 @@ window.addEventListener("load", function(event) {
   }, 200)
 });
 
-//information section showing up effect
+//menu section effect
 var section = document.getElementsByClassName("section");
-var sectionName = document.getElementsByClassName("sectionName");
+var menuSection = document.getElementsByClassName("menuSection");
 
-for (var i = 0; i < section.length; i++) {
-  hoverSection(i);
+menuSection[2].onmouseover = function(){
+  for (var i = 0; i < section.length; i++) {
+        section[i].classList.remove("hover");
+      }
+      section[0].classList.add("hover");
+      this.style.animation = "menuEffect .5s forwards";
+}
+menuSection[0].onmouseover = function(){
+  for (var i = 0; i < section.length; i++) {
+        section[i].classList.remove("hover");
+      }
+      section[1].classList.add("hover");
+      this.style.animation = "menuEffect .5s forwards";
+}
+menuSection[3].onmouseover = function(){
+  for (var i = 0; i < section.length; i++) {
+        section[i].classList.remove("hover");
+      }
+      section[2].classList.add("hover");
+      this.style.animation = "menuEffect .5s forwards";
+}
+menuSection[1].onmouseover = function(){
+  for (var i = 0; i < section.length; i++) {
+        section[i].classList.remove("hover");
+      }
+      section[3].classList.add("hover");
+      this.style.animation = "menuEffect .5s forwards";
+}
+menuSection[4].onmouseover = function(){
+  for (var i = 0; i < section.length; i++) {
+        section[i].classList.remove("hover");
+      }
+      section[4].classList.add("hover");
+      this.style.animation = "menuEffect .5s forwards";
 }
 
-function hoverSection() {
-  if (i == 0) {
-    section[i].onmouseover = function() {
-      for (j = 0; j < section.length; j++) {
-        section[j].classList.remove("hover");
-      }
-      this.classList.add("hover");
-      sectionName[2].style.visibility = "visible";
-      sectionName[2].style.animation = "sectionName 3s";
-    }
-  } else if (i == 1) {
-    section[i].onmouseover = function() {
-      for (j = 0; j < section.length; j++) {
-        section[j].classList.remove("hover");
-      }
-      this.classList.add("hover");
-      sectionName[0].style.visibility = "visible";
-      sectionName[0].style.animation = "sectionName 3s";
-    }
-  } else if (i == 2) {
-    section[i].onmouseover = function() {
-      for (j = 0; j < section.length; j++) {
-        section[j].classList.remove("hover");
-      }
-      this.classList.add("hover");
-      sectionName[3].style.visibility = "visible";
-      sectionName[3].style.animation = "sectionName 3s";
-    }
-  } else if (i == 3) {
-    section[i].onmouseover = function() {
-      for (j = 0; j < section.length; j++) {
-        section[j].classList.remove("hover");
-      }
-      this.classList.add("hover");
-      sectionName[1].style.visibility = "visible";
-sectionName[1].style.animation = "sectionName 3s";
-    }
-  } else if (i == 4) {
-    section[i].onmouseover = function() {
-      for (j = 0; j < section.length; j++) {
-        section[j].classList.remove("hover");
-      }
-      this.classList.add("hover");
-      sectionName[4].style.visibility = "visible";
-sectionName[4].style.animation = "sectionName 3s";
-    }
+for (var j = 0; j < menuSection.length; j++) {
+  menuSection[j].onmouseout = function(){
+    this.style.animation = "";
   }
 }
+
+// for (var i = 0; i < section.length; i++) {
+//   hoverSection(i);
+// }
+//
+// function hoverSection() {
+//   if (i == 0) {
+//     section[i].onmouseover = function() {
+//       for (j = 0; j < section.length; j++) {
+//         section[j].classList.remove("hover");
+//       }
+//       this.classList.add("hover");
+//       informationSection[2].style.visibility = "visible";
+//       informationSection[2].style.animation = "informationSection 2s";
+//     }
+//   } else if (i == 1) {
+//     section[i].onmouseover = function() {
+//       for (j = 0; j < section.length; j++) {
+//         section[j].classList.remove("hover");
+//       }
+//       this.classList.add("hover");
+//       informationSection[0].style.visibility = "visible";
+//       informationSection[0].style.animation = "informationSection 2s";
+//     }
+//   } else if (i == 2) {
+//     section[i].onmouseover = function() {
+//       for (j = 0; j < section.length; j++) {
+//         section[j].classList.remove("hover");
+//       }
+//       this.classList.add("hover");
+//       informationSection[3].style.visibility = "visible";
+//       informationSection[3].style.animation = "informationSection 2s";
+//     }
+//   } else if (i == 3) {
+//     section[i].onmouseover = function() {
+//       for (j = 0; j < section.length; j++) {
+//         section[j].classList.remove("hover");
+//       }
+//       this.classList.add("hover");
+//       informationSection[1].style.visibility = "visible";
+// informationSection[1].style.animation = "informationSection 2s";
+//     }
+//   } else if (i == 4) {
+//     section[i].onmouseover = function() {
+//       for (j = 0; j < section.length; j++) {
+//         section[j].classList.remove("hover");
+//       }
+//       this.classList.add("hover");
+//       informationSection[4].style.visibility = "visible";
+// informationSection[4].style.animation = "informationSection 2s";
+//     }
+//   }
+// }
+
+//studio logo link
+var logo = document.getElementById("logo");
+var menu = document.getElementById("menu");
+var title = document.getElementById("title");
+
+function toggleMenu(){
+  if (menu.style.display!="block"){
+    title.style.display="none";
+    menu.style.display="block";
+  }
+  else {
+    title.style.display="block";
+    menu.style.display="none";
+  }
+}
+
+logo.addEventListener("click", toggleMenu);
